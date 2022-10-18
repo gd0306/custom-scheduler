c3d1e2142ea7bd6db06947228a077243133f4308
guodong <guodong@weidian.com>
2022-10-18 20:30:30 +0800

    init scheduler

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"k8s.io/component-base/logs"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	command := app.NewSchedulerCommand(
+		app.WithPlugin(sample.Name, sample.New),
+	)
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+}
