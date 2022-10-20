519ad8b1a3b33c3b5cd9f67abdba1753736d2bf3
郭栋 <gd@guodongdeMacBook-Pro.local>
2022-10-20 23:53:06 +0800

    success run

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -12,6 +12,12 @@ import (
 // 插件名称
 const Name = "custom-scheduler"
 
+var (
+	_ framework.PreFilterPlugin = &Custom{}
+	_ framework.FilterPlugin    = &Custom{}
+	_ framework.PreBindPlugin   = &Custom{}
+)
+
 type Args struct {
 	FavoriteColor  string `json:"favorite_color,omitempty"`
 	FavoriteNumber int    `json:"favorite_number,omitempty"`
@@ -27,15 +33,30 @@ func (c *Custom) Name() string {
 	return Name
 }
 
-func (c *Custom) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
-	klog.V(3).Infof("prefilter pod: %v", pod.Name)
+func (c *Custom) PreFilter(ctx context.Context, state *framework.CycleState, p *v1.Pod) (*framework.PreFilterResult, *framework.Status) {
+	klog.V(3).Infof("prefilter pod: %v", p.Name)
+	return nil, framework.NewStatus(framework.Success, "")
+}
+
+func (c *Custom) PreFilterExtensions() framework.PreFilterExtensions {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c *Custom) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeInfo.Node().Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
-func (c *Custom) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
-	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
-	return framework.NewStatus(framework.Success, "")
-}
+//func (c *Custom) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
+//	klog.V(3).Infof("prefilter pod: %v", pod.Name)
+//	return framework.NewStatus(framework.Success, "")
+//}
+
+//func (c *Custom) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
+//	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
+//	return framework.NewStatus(framework.Success, "")
+//}
 
 func (c *Custom) PreBind(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
 	if nodeInfo, err := c.handle.SnapshotSharedLister().NodeInfos().Get(nodeName); err != nil {
